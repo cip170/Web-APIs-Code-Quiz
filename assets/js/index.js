b7b8b6c7e777f9d8976290f8abd98a1016027f53
cip170 <charlottephilipps@gmail.com>
2023-07-09 10:37:27 +1000

    added js questions

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+const myQuestions = [
+    {
+        text: 'Which of the following keywords is used to define a variable',
+        options: {
+            a: 'const',
+            b: 'var',
+            c: 'let',
+            d: 'All of the above'
+        },
+        answer: 'd'
+    },
+    {
+        text: 'Who said "onions have layers"?',
+        options: {
+            a: 'Donkey',
+            b: 'Shrek',
+            c: 'Lord Farquaad',
+            d: 'Fiona',
+        },
+        answer: 'b'
+    },
+    {
+        text: "What is Jude's favourite colour",
+        options: {
+            a: 'Orange',
+            b: 'Yellow',
+            c: 'Purple',
+            d: 'Green',
+        },
+        answer: 'c'
+    },
+]
