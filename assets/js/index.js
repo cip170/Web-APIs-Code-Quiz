5ebd43144175814ac2a9f5e290e4f23848868f67
cip170 <charlottephilipps@gmail.com>
2023-07-09 16:35:12 +1000

    added css styling to welcome page and question box - edited js to include question numbers and a reset game button

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,6 +7,9 @@ var secondsEl = document.querySelector('#seconds');
 var timeSpentEl = document.querySelector('#timeSpent');
 var scoreEl = document.querySelector('#score')
 var enterScoreForm = document.querySelector('#enterScoreForm')
+var tableBody = document.querySelector("#tableBody");
+var clearBtn = document.querySelector('#clear');
+var restartGame = document.querySelector('#restartGame')
 
 var currentQuestion = 0;
 var secondsElapsed = 0;
@@ -15,38 +18,71 @@ var score = 0;
 
 startButton.addEventListener("click", startGame);
 enterScoreForm.addEventListener('submit', recordScore)
-
+clearBtn.addEventListener('click', ()=>{
+    localStorage.setItem('quizScores', JSON.stringify([]))
+    renderTable()
+})
 
 const myQuestions = [
     {
-        text: 'Which of the following keywords is used to define a variable',
+        text: "Which of the following keywords is used to define a variable",
         options: {
-            a: 'const',
-            b: 'var',
-            c: 'let',
-            d: 'All of the above'
+            a: "const",
+            b: "var",
+            c: "let",
+            d: "All of the above",
         },
         answer: 'd'
     },
     {
-        text: 'Who said "onions have layers"?',
+        text: "When an operator's value is 'NULL', the typeof returned by the unary operator is - ",
         options: {
-            a: 'Donkey',
-            b: 'Shrek',
-            c: 'Lord Farquaad',
-            d: 'Fiona',
+            a: "Boolean",
+            b: "Object",
+            c: "Integer",
+            d: "Undefined",
         },
         answer: 'b'
     },
     {
-        text: "What is Jude's favourite colour",
+        text: "What does the JavaScript 'debugger' statement do?",
         options: {
-            a: 'Orange',
-            b: 'Yellow',
-            c: 'Purple',
-            d: 'Green',
+            a: "It acts as a breakpoint in the program",
+            b: "It will debug error in the current statement if any",
+            c: "It will debug the errors in the program at runtime",
+            d: "It will solve all errors found in the program",
+        },
+        answer: 'a'
+    },
+    {
+        text: "What function is used to convert an object into a JSON string in JavaScript?",
+        options: {
+            a: "convert()",
+            b: "stringify()",
+            c: "parse()",
+            d: "None of the above",
+        },
+        answer: 'b'
+    },
+    {
+        text: "What function would stop an interval timer in JavaScript?",
+        options: {
+            a: "intervalOver",
+            b: "clearTimer",
+            c: "clearInterval",
+            d: "intervalClear",
         },
         answer: 'c'
+    },
+    {
+        text: "What is the purpose of the querySelector method in JavaScript?",
+        options: {
+            a: "To search and return the first element within the document that matches the given selector",
+            b: "To select all elements within the document that match the given selector",
+            c: "To modify the document's properties of the given element",
+            d: "To add event listeners to elements",
+        },
+        answer: 'a'
     },
 ]
 
@@ -68,21 +104,25 @@ function startGame() {
 
 function renderQuestion() {
     questionEl.innerHTML = `
-        <h3>${myQuestions[currentQuestion].text}</h3>
+        <h3>Question ${currentQuestion + 1}: ${myQuestions[currentQuestion].text}</h3>
+        <div class="options-container">
             <ul>
-            <li>
+            <li>a.
             <button class="option-btn" id="a">${myQuestions[currentQuestion].options.a}</button>
             </li>
-            <li>
+            <li>b.
             <button class="option-btn" id="b">${myQuestions[currentQuestion].options.b}</button>
             </li>
-            <li>
+            </ul>
+            <ul>
+            <li>c.
             <button class="option-btn" id="c">${myQuestions[currentQuestion].options.c}</button>
             </li>
-            <li>
+            <li>d.
             <button class="option-btn" id="d">${myQuestions[currentQuestion].options.d}</button>
             </li>
             </ul>
+            </div>
     `
 
     var optionButtonsList = (document.querySelectorAll('.option-btn'))
@@ -120,7 +160,39 @@ function gameOver() {
 
     timeSpentEl.textContent = secondsElapsed
     scoreEl.textContent = score
+
+    renderTable()
 }
+
+function renderTable() {
+    // read local Storage
+    const scoresFromStorage =
+        JSON.parse(
+            localStorage.getItem('quizScores')
+        )
+        || []
+
+    tableBody.innerHTML = ''
+
+    scoresFromStorage.forEach((entry) => {
+        var row = document.createElement('tr')
+        row.innerHTML = `
+            <td>${entry.initials}</td>
+            <td>${entry.score}</td>
+            `
+        tableBody.append(row)
+    })
+}
+
+restartGame.addEventListener("click", function() {
+    welcomeEl.classList.remove("invisible");
+    endGameEl.classList.add("invisible");
+
+    currentQuestion = 0;
+    score = 0;
+
+    clearInterval(timer);
+});
 
 function recordScore(e) {
     e.preventDefault()
@@ -142,5 +214,8 @@ function recordScore(e) {
     // write localStorage
     localStorage.setItem('quizScores', JSON.stringify(scoresFromStorage))
 
-}
+    enterScoreForm.reset()
 
+    renderTable()
+
+}
